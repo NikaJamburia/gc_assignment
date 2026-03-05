0f39d3b9e9afc31ac519f6f1e6f9b4ff6a1608d6
nika-jamburia <nika.jamburia@twino.ge>
2026-03-05 16:22:03 +0400

    Added some comments

--- src/main/java/eu/twino/loans/core/georgian_card_assignment/TransactionsController.java
+++ src/main/java/eu/twino/loans/core/georgian_card_assignment/TransactionsController.java
@@ -9,11 +9,10 @@ import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.*;
+import java.util.stream.Collectors;
 
 /**
  * ტესტებს ვეღარ ვასწრებ, შეიძლება რაღაცეებმა არ იმუშაუს, მარა ლოგიკა მგონი გასაგებია :)
@@ -23,56 +22,42 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * თუ გვინდა ბოლო 60 წამის სტატისტიკის აღებას, ვიღებთ მერჩანთისთვის ბოლო ჩანაწერს, რომელიც დაემატა ბოლო 60 წამის განმავლობაში
  * თუ რეინჯით ვიღებთ, უბრალოდ ვფილტრავთ და ასევე ბოლო ჩანაწერს ვიღებთ
  *
- * თუ რომელიმე მერჩანთისთვის სტატისტიკის ჩანაწერების რაოდენობა 10ზე მეტია, იღვიძებს ტრედი რომელიც წინა ჩანაწერებს შლის და მარტო 1ს ტოვებს,
- * ყველაზე ბოლო რომელიცაა დამატებული
+ * ყოველ 5 წუთში ვშლით ტრანზაქციებს, რომელიც 5 წუთზე ძველია
  */
 @RestController
 public class TransactionsController {
 
-    private static final int STATISTICS_THRESHOLD = 10;
-
-    private final Map<String, Set<MerchantStatistics>> merchantsStatistics = new HashMap<>();
+    private final Map<String, Set<MerchantStatistics>> merchantsStatistics = new ConcurrentHashMap<>();
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Condition merchantsStatisticsFull = lock.writeLock().newCondition();
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-    private final Thread backgroundStatisticsCleaningThread = new Thread(() -> {
-        while (true) {
-            var writeLock = lock.writeLock();
-            writeLock.lock();
+    private final Lock lock = new ReentrantLock();
+    private final Condition statisticsCleared = lock.newCondition();
+    private final AtomicBoolean clearingInProgress = new AtomicBoolean(false);
 
-            try {
-                // იცდის სანამ რომელიმე მერჩანთის სტატისტიკა 10ზე მეტი იქნება
-                // ბოლოს მივხვდი რომ ეს მთლად ეფექტური არაა, რადგან გაღვიძების მერე ეს ტრედი კონკურენციაში შევა ექზეკიუტორის ტრედთან,
-                // და შეიძლება ვერ მოასწროს დალოქვა, რის გამოც ლისტები შიეძლება ვერ გაიწმინდოს თავის დროზე. მგონი დიდი პრობლემა არაა და
-                // დამატებითი ქონდიშენითაც შეიძლება მოგვარება, ან რამე ფლეგით
-                merchantsStatisticsFull.await();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            try {
-                // ყველა მერჩანთს, რომლის სტატისტიკის რექორდები 10ზე მეტია, უშლის წინებს და ტოვებს მარტო ბოლოს
-                merchantsStatistics.forEach((key, value) -> {
-                    if (value.size() > STATISTICS_THRESHOLD) {
-                        var latestStat = value.stream()
-                                .max(Comparator.comparing(MerchantStatistics::timestamp))
-                                .orElseThrow();
-                        var newSet = new HashSet<MerchantStatistics>();
-                        newSet.add(latestStat);
-                        merchantsStatistics.put(key, newSet);
-                    }
-                });
-            } finally {
-                writeLock.unlock();
-            }
-        }
-    });
 
     @EventListener(ApplicationReadyEvent.class)
     public void startBackgroundThread() {
-        backgroundStatisticsCleaningThread.start();
+        scheduler.scheduleAtFixedRate(() -> {
+            var fiveMinsAgo = Instant.now().minusSeconds(60 * 5);
+
+            lock.lock();
+            try {
+                clearingInProgress.set(true);
+                merchantsStatistics.forEach((key, value) -> {
+                    var filtered = value
+                            .stream()
+                            .filter(st -> st.timestamp().isAfter(fiveMinsAgo))
+                            .collect(Collectors.toSet());
+                    merchantsStatistics.put(key, filtered);
+                });
+            } finally {
+                clearingInProgress.set(false);
+                statisticsCleared.signal();
+                lock.unlock();
+            }
+        }, 5, 5, TimeUnit.MINUTES);
     }
 
     @PostMapping("/transactions")
@@ -87,18 +72,11 @@ public class TransactionsController {
 
     @GetMapping("/stats")
     MerchantStatistics getStats(@RequestParam("merchantId") String merchantId) {
-        // აქ გვინდა რიდ ლოქი, რომ არ ველოდოთ სტატისტიკის განახლების ტასკს და გვქონდეს ბოლო განახლების რეზულტატი
-        var readLock = lock.readLock();
-        readLock.lock();
-        try {
-            return merchantsStatistics.get(merchantId)
-                    .stream()
-                    .filter(stat -> stat.timestamp().isAfter(Instant.now().minusSeconds(60)))
-                    .max(Comparator.comparing(MerchantStatistics::timestamp))
-                    .orElseThrow(() -> new IllegalArgumentException("No statistics for merchant"));
-        } finally {
-            readLock.unlock();
-        }
+        return merchantsStatistics.get(merchantId)
+                .stream()
+                .filter(stat -> stat.timestamp().isAfter(Instant.now().minusSeconds(60)))
+                .max(Comparator.comparing(MerchantStatistics::timestamp))
+                .orElseThrow(() -> new IllegalArgumentException("No statistics for merchant"));
 
     }
 
@@ -108,95 +86,61 @@ public class TransactionsController {
             @RequestParam("from") Instant from,
             @RequestParam("to") Instant to
     ) {
-        // აქ გვინდა რიდ ლოქი, რომ არ ველოდოთ სტატისტიკის განახლების ტასკს და გვქონდეს ბოლო განახლების რეზულტატი.
-        var readLock = lock.readLock();
-        readLock.lock();
-        try {
-            return merchantsStatistics.get(merchantId)
-                    .stream()
-                    .filter(stat ->
-                            stat.timestamp().isAfter(from) && stat.timestamp().isBefore(to)
-                    )
-                    .max(Comparator.comparing(MerchantStatistics::timestamp))
-                    .orElseThrow(() ->new IllegalArgumentException("No statistics for merchant"));
-        } finally {
-            readLock.unlock();
-        }
+        return merchantsStatistics.get(merchantId)
+                .stream()
+                .filter(stat ->
+                        stat.timestamp().isAfter(from) && stat.timestamp().isBefore(to)
+                )
+                .max(Comparator.comparing(MerchantStatistics::timestamp))
+                .orElseThrow(() ->new IllegalArgumentException("No statistics for merchant"));
     }
 
     private void updateStatistics(TransactionData transactionData) {
-        // ვიღებთ ლოკს. უფრო ეფექტური რომ იყოს, შეიძლება ამის გაშვება თრედ პულში და არა 1 ტრედზე, და თითო მერჩანტისთვის
-        // ცალკე ლოქის ქონა. მაგ შემთხვევაში, 2 თრედს შეეძლებოდა სხვადასხვა მერჩანთების სტატისტიკის განახლება დალოდების გარეშე
-        var writeLock = lock.writeLock();
-        writeLock.lock();
+        // ველოდებით ძველი სტატისტიკის წაშლას
+        lock.lock();
 
         try {
-            var stats = merchantsStatistics.get(transactionData.merchantId());
-
-            if (stats == null) {
-                var newStat = new MerchantStatistics(
-                        transactionData.amount(),
-                        1,
-                        transactionData.amount(),
-                        transactionData.amount(),
-                        transactionData.amount(),
-                        transactionData.timestamp()
-                );
-                Set<MerchantStatistics> value = new HashSet<>();
-                value.add(newStat);
-                merchantsStatistics.put(transactionData.merchantId(), value);
-            } else {
-                var lastRecord = stats
-                        .stream()
-                        .max(Comparator.comparing(MerchantStatistics::timestamp))
-                        .orElseThrow();
-                BigDecimal newTotal = lastRecord.totalAmount().add(transactionData.amount());
-                int newCount = lastRecord.transactionsCount() + 1;
-
-                var newStat = new MerchantStatistics(
-                        newTotal,
-                        newCount,
-                        transactionData.amount().min(lastRecord.minAmount()),
-                        transactionData.amount().max(lastRecord.maxAmount()),
-                        newTotal.divide(new BigDecimal(newCount), RoundingMode.HALF_DOWN),
-                        transactionData.timestamp()
-                );
-                stats.add(newStat);
-                if (stats.size() > STATISTICS_THRESHOLD) {
-                    // თუ ძაან ბევრი სტატისტიკა დაგროვდა მერჩანტზე, სიგნალს ვუგზავნით ბექრაუნდ ტრედს, რომელიც წაშლის
-                    merchantsStatisticsFull.signal();
-                }
+            while (clearingInProgress.get()) {
+                statisticsCleared.await();
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
 
-    }
+        merchantsStatistics.computeIfAbsent(transactionData.merchantId(), (key) -> {
+            var newStat = new MerchantStatistics(
+                    transactionData.amount(),
+                    1,
+                    transactionData.amount(),
+                    transactionData.amount(),
+                    transactionData.amount(),
+                    transactionData.timestamp()
+            );
+            Set<MerchantStatistics> value = new HashSet<>();
+            value.add(newStat);
+            return value;
+        });
 
-//    private MerchantStatistics calculateStats(List<TransactionData> transactionData) {
-//        BigDecimal totalAmount = new BigDecimal(0);
-//        int count = 0;
-//        BigDecimal min = null;
-//        BigDecimal max = BigDecimal.ZERO;
-//
-//        for (TransactionData tx : transactionData) {
-//            count++;
-//            totalAmount = totalAmount.add(tx.amount());
-//
-//            if (min == null) {
-//                min = tx.amount();
-//            } else {
-//                min = tx.amount().min(min);
-//            }
-//            max = tx.amount().max(tx.amount());
-//        }
-//
-//        return new MerchantStatistics(
-//            totalAmount,
-//            count,
-//            min,
-//            max,
-//            totalAmount.divide(new BigDecimal(count), RoundingMode.HALF_DOWN)
-//        );
-//    }
+        merchantsStatistics.computeIfPresent(transactionData.merchantId(),( key, value) -> {
+            var lastRecord = value
+                    .stream()
+                    .max(Comparator.comparing(MerchantStatistics::timestamp))
+                    .orElseThrow();
+            BigDecimal newTotal = lastRecord.totalAmount().add(transactionData.amount());
+            int newCount = lastRecord.transactionsCount() + 1;
+
+            var newStat = new MerchantStatistics(
+                    newTotal,
+                    newCount,
+                    transactionData.amount().min(lastRecord.minAmount()),
+                    transactionData.amount().max(lastRecord.maxAmount()),
+                    newTotal.divide(new BigDecimal(newCount), RoundingMode.HALF_DOWN),
+                    transactionData.timestamp()
+            );
+            value.add(newStat);
+            return value;
+        });
+    }
 }
